268afac8e99c8eca94abe5269f8d7303e736d0e9
Zachary Ferretti <zf66728@gmail.com>
2019-01-17 13:34:30 +0000

    Working on implimenting React

---
+++ My Own Website/Just Getting React Working/react_spa/src/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+ 
+class Main extends Component {
+  render() {
+    return (
+        <div>
+          <h1>Simple SPA<\h1>
+          <ul className = "Header">
+            <li><a href = "/">Home<\a><\li>
+            <li><a href = "/stuff">Stuff<\a><\li>
+            <li><a href = "/contact">Contact<\a><\li>
