0c49f4383e07be1185096a6e02e976cf8ab760cb
Zachary Ferretti <zf66728@gmail.com>
2019-01-17 14:34:24 +0000

    React Stuff

--- My Own Website/Just Getting React Working/react_spa/src/Main.js
+++ My Own Website/Just Getting React Working/react_spa/src/Main.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
+import {
+	Route,
+	NavLink,
+	HashRouter
+}from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
  
 class Main extends Component {
   render() {
     return (
+		<HashRouter>
         <div>
-          <h1>Simple SPA<\h1>
-          <ul className = "Header">
-            <li><a href = "/">Home<\a><\li>
-            <li><a href = "/stuff">Stuff<\a><\li>
-            <li><a href = "/contact">Contact<\a><\li>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/stuff" component={Stuff}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+        </div>
+		</HashRouter>
+    );
+  }
+}
+ 
+export default Main;
